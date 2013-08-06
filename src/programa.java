1cf5a0c4f200e327328107cac61328367a914473
juan pablo <juan_92psoada@hotmail.com>
2013-08-06 12:25:33 -0500

    este es el segundo proyecto

---
+++ src/programa.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+
+public class programa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s=JOptionPane.showInputDialog("juan");
+
+	}
+
+}
